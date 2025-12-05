4b1ef66cbe3fc51264b0446a8d5d60eb9c366a6d
xtrevad <trevor.adelson@gmail.com>
2025-12-06 02:02:48 +1100

    FEAT: leader-vst and leader-hst for vertical/horizontal terminal splits and auto-resize

--- init.lua
+++ init.lua
@@ -199,6 +199,20 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+vim.keymap.set('n', '<leader>vst', function()
+  vim.cmd 'vsplit | terminal'
+  local new_win = vim.api.nvim_get_current_win()
+  vim.api.nvim_set_current_win(new_win)
+  vim.cmd 'vertical resize -40'
+end, { desc = 'Vertical split terminal' })
+
+vim.keymap.set('n', '<leader>hst', function()
+  vim.cmd 'split | terminal'
+  local new_win = vim.api.nvim_get_current_win()
+  vim.api.nvim_set_current_win(new_win)
+  vim.cmd 'resize -20'
+end, { desc = 'Horizontal split terminal' })
+
 -- NOTE: Some terminals have colliding keymaps or are not able to send distinct keycodes
 -- vim.keymap.set("n", "<C-S-h>", "<C-w>H", { desc = "Move window to the left" })
 -- vim.keymap.set("n", "<C-S-l>", "<C-w>L", { desc = "Move window to the right" })
